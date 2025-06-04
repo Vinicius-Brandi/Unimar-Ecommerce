b72cc2f8604f5615b566d8db2bf3f4650c42acad
Vinicius-Brandi <vviniciusbrandi@gmail.com>
2025-06-04 16:36:31 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ export PYTHONPATH="/app:$PYTHONPATH"
 
 # Aplica migrações do banco de dados
 echo "Applying database migrations..."
-python manage.py migrate --noinput || exit 1 # Adicionado --noinput e exit 1 para falha de migração
+python manage.py migrate --noinput || { echo "Database migrations failed!" && exit 1; }
 
 # Inicia o servidor Gunicorn
 echo "Starting Gunicorn server..."
 # A última parte do comando do Gunicorn deve ser 'Core.wsgi:application'
-exec gunicorn Core.wsgi:application --bind 0.0.0.0:8000 --workers 3 # Exemplo, ajuste conforme necessário
+exec gunicorn Core.wsgi:application --bind 0.0.0.0:8000 --workers 3
